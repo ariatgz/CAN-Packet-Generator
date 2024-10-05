ef93ca600c8439e1b2fec08d90ff78380fda0987
Arya Taghizadeh <100978388+ariatgz@users.noreply.github.com>
2024-10-05 16:38:05 -0400

    Add files via upload

--- CAN Packet Generator/CAN Packet Generator/Source.cpp
+++ CAN Packet Generator/CAN Packet Generator/Source.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void kelly_1_generator(short direction, unsigned short speed,short error_code);
+void kelly_1_generator(short direction, unsigned int speed,short error_code);
 void kelly_2_generator(unsigned short voltage, unsigned short current, unsigned short temp_m, unsigned short temp_c);
 
 void kelly_1_collector();
@@ -21,20 +21,37 @@ int main() {
 
 	int selected_motor;
 
-	cout << "Which motor do you want create a packet for?" << endl;
-	cout << "1) Kelly 1" << endl;
-	cout << "2) Kelly 2" << endl;
+	
 
-	cin >> selected_motor;
+		try
+		{
+			cout << "Which motor do you want create a packet for?" << endl;
+			cout << "1) Kelly 1" << endl;
+			cout << "2) Kelly 2" << endl;
 
-	assert(selected_motor == 1 || selected_motor == 2);
+			cin >> selected_motor;
 
-	if (selected_motor == 1) {
-		kelly_1_collector();
-	}
-	else if (selected_motor == 2) {
-		kelly_2_collector();
-	}
+			assert(selected_motor == 1 || selected_motor == 2);
+
+			if (selected_motor == 1) {
+				kelly_1_collector();
+			}
+			else if (selected_motor == 2) {
+				kelly_2_collector();
+			}
+
+		}
+		catch (const std::exception&)
+		{
+			cout << "Some value was invalid!!" << endl;
+			
+		}
+
+		
+
+	
+
+	
 
 
 
@@ -51,19 +68,28 @@ void kelly_1_collector() {
 	unsigned int speed;
 	short error;
 
+	try {
+		cout << "What will be the direction of the motor?" << endl;
+		cout << "1) Forward" << endl;
+		cout << "2) Reverse" << endl;
+		cout << "3) Neutral" << endl;
+		cin >> direction;
 
-	cout << "What will be the direction of the motor?" << endl;
-	cout << "1) Forward" << endl;
-	cout << "2) Reverse" << endl;
-	cout << "3) Neutral" << endl;
-	cin >> direction;
+		cout << "What will be the RPM in decimal? less than " << MAX_SPEED << endl;
+		cin >> speed;
 
-	cout << "What will be the RPM in decimal? less than "<<MAX_SPEED << endl;
-	cin >> speed;
+		cout << "Any Error codes? 0 by default for no errors" << endl;
+		cin >> error;
 
-	cout << "Any Error codes? 0 by default for no errors" << endl;
-	cin >> error;
 
+	}
+	catch (const exception& e) {
+
+		cout<< e.what()<<endl;
+		throw;
+	}
+
+	
 	kelly_1_generator(direction,speed,error);
 
 
@@ -71,20 +97,18 @@ void kelly_1_collector() {
 
 }
 
-void kelly_1_generator(short direction, unsigned short speed, short error_code = 0) {
+void kelly_1_generator(short direction, unsigned int speed, short error_code = 0) {
 
 	
 
 	
 
-	cout << "Frame is {" << endl;
-	cout << "ID: CAN_ID_EXT" << endl;
-	cout << "data_length: sizeof(data)" << endl;
-	cout << "data: 00000000";
+	cout << " ***************" << endl;
+	cout << "Payload: 0x00000000";
 	printf("%02x", error_code);
-	printf("%02x", speed);
+	printf("%04x", speed);
 	printf("%02x", direction);
-	cout <<" }"<< endl;
+	cout << endl << " ***************" << endl;
 
 
 
@@ -121,14 +145,12 @@ void kelly_2_generator(unsigned short  voltage, unsigned short  current, unsigne
 	temp_m *= 10;
 	temp_c *= 10;
 
-	cout << "Frame is {" << endl;
-	cout << "ID: CAN_ID_EXT" << endl;
-	cout << "data_length: sizeof(data)" << endl;
-	cout << "data:0x";
+	cout << " ***************" << endl;
+	cout << "Payload: 0x";
 	printf("%04x", temp_c);
 	printf("%04x", temp_m);
 	printf("%04x", current);
 	printf("%04x", voltage);
-	cout <<endl<< " }" << endl;
+	cout <<endl<< " ***************" << endl;
 }
 
